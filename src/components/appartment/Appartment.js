e7999f7d01aa7c82bba88da4ea0c243cf31b64b5
dionysis9901 <dionysis.koufis@gmail.com>
2020-05-09 18:58:23 +0300

    adds UI

--- src/components/appartment/Appartment.js
+++ src/components/appartment/Appartment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./appartment.scss";
 import { GrView } from "react-icons/gr";
 
@@ -11,12 +11,38 @@ const Appartment = ({
   image,
   maxpeople,
   nid,
+  viewDetails,
 }) => {
+  const [details, setDetails] = useState(true);
+
+  if (details) {
+    return (
+      <div className="appartmentUncensored">
+        <img className="appartment__image" src={image} alt="Appartment" />
+
+        <p className="appartment__title">{title}</p>
+
+        <button className="appartment__btn" onClick={() => setDetails(false)}>
+          <GrView />
+        </button>
+
+        <p className="appartment__price">From {price} € / night</p>
+        <p className="appartment__mainInfo">
+          {maxpeople} Guests • {bathrooms} Bathroom • {bedrooms} Bedrooms
+        </p>
+        <p className="appartment__amenities">{amenities}</p>
+
+        <button className="appartment__bookNowBtn">Book Now</button>
+      </div>
+    );
+  }
+
   return (
     <div className="appartment">
-      <img className="appartment__image" src={image} alt="Appartment Image" />
+      <img className="appartment__image" src={image} alt="Appartment" />
       <p className="appartment__title">{title}</p>
-      <button className="appartment__btn">
+
+      <button className="appartment__btn" onClick={() => setDetails(true)}>
         <GrView />
       </button>
     </div>
